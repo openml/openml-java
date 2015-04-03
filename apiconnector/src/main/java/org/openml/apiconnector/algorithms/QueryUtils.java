483e0a9300f6c10ed2b197f73bf93dd08e57c0f3
janvanrijn <janvanrijn@gmail.com>
2015-04-03 19:25:07 +1300

    added numbers from database function

--- apiconnector/src/main/java/org/openml/apiconnector/algorithms/QueryUtils.java
+++ apiconnector/src/main/java/org/openml/apiconnector/algorithms/QueryUtils.java
@@ -15,6 +15,17 @@ public class QueryUtils {
 
         return result;
     }
+    
+    public static double[] getNumbersFromDatabase( OpenmlConnector apiconnector, String sql ) throws Exception {
+        JSONArray runJson = (JSONArray) apiconnector.freeQuery(sql).get("data");
+
+        double[] result = new double[runJson.length()];
+        for( int i = 0; i < runJson.length(); ++i ) {
+            result[i] = ( (JSONArray) runJson.get( i )).getDouble( 0 );
+        }
+
+        return result;
+    }
 
     public static double getIntFromDatabase( OpenmlConnector apiconnector, String sql ) throws Exception {
         int[] result = getIdsFromDatabase( apiconnector, sql );
