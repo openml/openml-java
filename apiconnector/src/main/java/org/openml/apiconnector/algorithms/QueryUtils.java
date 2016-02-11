5f080064032e49d9f18aeaf152e8ce5ed7e14160
janvanrijn <janvanrijn@gmail.com>
2016-02-11 15:00:46 +1300

    update query utils

--- apiconnector/src/main/java/org/openml/apiconnector/algorithms/QueryUtils.java
+++ apiconnector/src/main/java/org/openml/apiconnector/algorithms/QueryUtils.java
@@ -1,34 +1,49 @@
 package org.openml.apiconnector.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.openml.apiconnector.io.OpenmlConnector;
 
 public class QueryUtils {
 
-    public static int[] getIdsFromDatabase( OpenmlConnector apiconnector, String sql ) throws Exception {
-        JSONArray runJson = (JSONArray) apiconnector.freeQuery(sql).get("data");
+	public static Integer[] getIdsFromDatabase(OpenmlConnector apiconnector, String sql) throws Exception {
+		JSONArray runJson = (JSONArray) apiconnector.freeQuery(sql).get("data");
 
-        int[] result = new int[runJson.length()];
-        for( int i = 0; i < runJson.length(); ++i ) {
-            result[i] = (int) ( (JSONArray) runJson.get( i )).getDouble( 0 );
-        }
+		Integer[] result = new Integer[runJson.length()];
+		for (int i = 0; i < runJson.length(); ++i) {
+			result[i] = (int) ((JSONArray) runJson.get(i)).getDouble(0);
+		}
 
-        return result;
-    }
-    
-    public static double[] getNumbersFromDatabase( OpenmlConnector apiconnector, String sql ) throws Exception {
-        JSONArray runJson = (JSONArray) apiconnector.freeQuery(sql).get("data");
+		return result;
+	}
 
-        double[] result = new double[runJson.length()];
-        for( int i = 0; i < runJson.length(); ++i ) {
-            result[i] = ( (JSONArray) runJson.get( i )).getDouble( 0 );
-        }
+	public static double[] getNumbersFromDatabase(OpenmlConnector apiconnector, String sql) throws Exception {
+		JSONArray runJson = (JSONArray) apiconnector.freeQuery(sql).get("data");
 
-        return result;
-    }
+		double[] result = new double[runJson.length()];
+		for (int i = 0; i < runJson.length(); ++i) {
+			result[i] = ((JSONArray) runJson.get(i)).getDouble(0);
+		}
 
-    public static double getIntFromDatabase( OpenmlConnector apiconnector, String sql ) throws Exception {
-        int[] result = getIdsFromDatabase( apiconnector, sql );
-        return result[0];
-    }
+		return result;
+	}
+
+	public static double getIntFromDatabase(OpenmlConnector apiconnector, String sql) throws Exception {
+		Integer[] result = getIdsFromDatabase(apiconnector, sql);
+		return result[0];
+	}
+	
+	public static Map<Integer,String> getMapFromDatabase(OpenmlConnector apiconnector, String sql) throws Exception {
+		Map<Integer,String> result = new HashMap<Integer, String>();
+		JSONArray runJson = (JSONArray) apiconnector.freeQuery(sql).get("data");
+		
+		for (int i = 0; i < runJson.length(); ++i) {
+			JSONArray row = ((JSONArray) runJson.get(i));
+			result.put(row.getInt(0), row.getString(1));
+		}
+		
+		return result;
+	}
 }
