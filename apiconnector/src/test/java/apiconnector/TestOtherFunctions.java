3ce39729689ac5b64dc10950a192871118d130ae
Jan van Rijn <janvanrijn@gmail.com>
2016-09-22 15:24:37 +0200

    Extended task check

--- apiconnector/src/test/java/apiconnector/TestOtherFunctions.java
+++ apiconnector/src/test/java/apiconnector/TestOtherFunctions.java
@@ -1,12 +1,13 @@
 package apiconnector;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
+import org.openml.apiconnector.algorithms.TaskInformation;
 import org.openml.apiconnector.io.OpenmlConnector;
-import org.openml.apiconnector.xstream.XstreamXmlMapping;
-
-import com.thoughtworks.xstream.XStream;
+import org.openml.apiconnector.xml.DataQuality;
+import org.openml.apiconnector.xml.Task;
 
 public class TestOtherFunctions {
 
@@ -14,14 +15,23 @@ public class TestOtherFunctions {
 	private static final String url = "http://test.openml.org/";
 	private static final String session_hash = "d488d8afd93b32331cf6ea9d7003d4c3";
 	private static final OpenmlConnector client = new OpenmlConnector(url,session_hash);
-	private static final XStream xstream = XstreamXmlMapping.getInstance();
-
+	private static final Integer taskId = 1;
+	
+	
 	@Test
 	public void testApiAdditional() {
 		try {
-			client.taskGet(1);
+			Task t = client.taskGet(taskId);
+			
+			String splitsUrl = TaskInformation.getEstimationProcedure(t).getData_splits_url();
 			
 			
+			Integer dataId = TaskInformation.getSourceData(t).getData_set_id();
+			String[] splits = OpenmlConnector.getStringFromUrl(splitsUrl).split("\n");
+			DataQuality dq = client.dataQualities(dataId);
+			int numInstances = (int) Double.parseDouble(dq.getQualitiesMap().get("NumberOfInstances"));
+			
+			assertTrue(splits.length > numInstances); // basic check
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("Test failed: " + e.getMessage());
