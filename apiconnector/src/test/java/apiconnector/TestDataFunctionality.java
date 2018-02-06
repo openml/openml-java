78d2ccd999764cd849331e626c572a318c2db4c0
ArlindKadra <arlindkadra@gmail.com>
2018-02-06 20:11:07 +0100

    Refactored code

--- apiconnector/src/test/java/apiconnector/TestDataFunctionality.java
+++ apiconnector/src/test/java/apiconnector/TestDataFunctionality.java
@@ -135,10 +135,7 @@ public class TestDataFunctionality {
 	@Test
 	public void testApiUploadDownload() throws Exception {
 		client_write.setVerboseLevel(1);
-		DataSetDescription dsd = new DataSetDescription("test", "Unit test should be deleted", "arff", "class");
-		String dsdXML = xstream.toXML(dsd);
-		System.out.println(dsdXML);
-		File description = Conversion.stringToTempFile(dsdXML, "test-data", "arff");
+		File description = createTestDatasetDescription();
 		File toUpload = new File(data_file);
 		UploadDataSet ud = client_write.dataUpload(description, toUpload);
 		DataTag dt = client_write.dataTag(ud.getId(), tag);
@@ -180,43 +177,45 @@ public class TestDataFunctionality {
 		assertTrue(ud.getId() == dd.get_id());
 	}
 	
+	private File createTestDatasetDescription() throws IOException {
+		
+		DataSetDescription dsd = new DataSetDescription("test", "Unit test should be deleted", "arff", "class");
+		String dsdXML = xstream.toXML(dsd);
+		return Conversion.stringToTempFile(dsdXML, "test-data", "arff");
+	}
+	
 	@Test
 	public void testUploadDataset() throws IOException {
 		
 		client_write.setVerboseLevel(1);
 		// Test XML description
-		DataSetDescription dsd = new DataSetDescription("test", "Unit test should be deleted", "arff", "class");
-		final String dsdXML = xstream.toXML(dsd);
+		final File description = createTestDatasetDescription();
 		Path path = Paths.get(DATASETPATH);
 		// Pass through each dataset on the directory
 		Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
 		@Override
 		public FileVisitResult visitFile(Path file, BasicFileAttributes attr) throws IOException {
 					
-			System.out.println(file.getFileName());
-			File description = Conversion.stringToTempFile(dsdXML, "test-data", "arff");
 			File toUpload = new File(file.toString());
 			int id = -1;
-			if(file.getFileName().toString().startsWith("invalid")) {
-				try {
-					UploadDataSet ud = client_write.dataUpload(description, toUpload);
-					id = ud.getId();
-					client_write.dataDelete(id);
-				} catch(Exception e) {
-					e.printStackTrace();
-				} finally {
+			// boolean to signal the validity of a dataset
+			boolean invalid = file.getFileName().toString().startsWith("invalid");
+			
+			try {
+				UploadDataSet ud = client_write.dataUpload(description, toUpload);
+				id = ud.getId();
+				// Only reached by a dataset (ARFF file) that gets uploaded.
+				client_write.dataDelete(id);
+			} catch(Exception e) {
+				e.printStackTrace();
+			} finally {
+				if(invalid) {
 					assertTrue(id == -1);
-				}
-			} else {
-				try {
-					UploadDataSet ud = client_write.dataUpload(description, toUpload);
-					id = ud.getId();
-					client_write.dataDelete(id);
-				} catch(Exception e ) {
-					e.printStackTrace();
-				} finally {
+				} else {
 					assertTrue(id != -1);
 				}
+				// Reset the dataset id
+				id = -1;
 			}
 			// Keep going through datasets
 			return FileVisitResult.CONTINUE;
