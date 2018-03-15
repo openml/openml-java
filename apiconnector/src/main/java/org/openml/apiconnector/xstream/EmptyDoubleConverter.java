8bead52875c1b3765c9eb6c85a2541bd72c507e7
janvanrijn <janvanrijn@gmail.com>
2018-03-14 21:09:51 -0400

    safer way of double conversion

--- apiconnector/src/main/java/org/openml/apiconnector/xstream/EmptyDoubleConverter.java
+++ apiconnector/src/main/java/org/openml/apiconnector/xstream/EmptyDoubleConverter.java
@@ -2,22 +2,15 @@ package org.openml.apiconnector.xstream;
 
 import org.apache.commons.lang3.StringUtils;
 
-import com.thoughtworks.xstream.converters.Converter;
-import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.converters.basic.DoubleConverter;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
-public class DoubleConverter implements Converter {
+public class EmptyDoubleConverter extends DoubleConverter {
 	// Handles empty Double tags (e.g., <oml:value /> for quality list)
 
 	public boolean canConvert(Class type) {
 		return type.equals(Double.class);
-	}
-
-	@Override
-	public void marshal(Object Obj, HierarchicalStreamWriter arg1, MarshallingContext arg2) {
-
 	}
 
 	public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
